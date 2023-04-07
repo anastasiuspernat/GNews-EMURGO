1af491f3b86cccdeba34a6d908c2c9256d2b994b
Anastasius <anastasiuspernat@gmail.com>
2023-04-07 00:09:21 -0700

    Added comments to code

--- gnews-api.js
+++ gnews-api.js
@@ -1,98 +1,162 @@
+/**
+ * GNews API
+ * Provides a simple API to fetch articles from GNews 
+ */
+
 // Import necessary libraries
 const express = require('express');
 const axios = require('axios');
 const NodeCache = require('node-cache');
 
-// Initialize NodeCache
+// Define API key
+// NOTE: You will need to get your own API key from https://gnews.io/
+// This one is for demonstration purposes only and will stop working after a while
+const API_KEY = '640e0a8b4d1671b80f383f439d4d9785';
+
+// Initialize NodeCache, will be used to store API responses and return them for subsequent requests
 const cache = new NodeCache();
 
 // Create Express app
 const app = express();
 
-const API_KEY = '640e0a8b4d1671b80f383f439d4d9785';
-
 // Define API endpoints
+
+/**
+ * GET /articles
+ * API endpoint to fetch articles from GNews API
+ * @param {number} n - number of articles to return
+ */
 app.get('/articles', async (req, res) => {
+
+  // Get n from query string - number of articles to return
   const { n } = req.query;
 
+  // Check if n is provided
   if (!n) {
     console.error("Please provide n (number of articles)");
     res.status(500).send('Please provide n (number of articles)');
   } else {
-  
+    // If n is provided, fetch articles from API and return them
+
     // Check cache first
+    // Key for cache is articles_n, where n is the number of articles to return
     const cacheKey = `articles_${n}`;
+
+    // Get cached data
     const cachedData = cache.get(cacheKey);
+
+    // If cached data exists, return it
     if (cachedData) {
       return res.json(cachedData);
     }
 
     try {
+      // If cached data doesn't exist, fetch data from API using axios
       const response = await axios.get(`https://gnews.io/api/v4/top-headlines?token=${API_KEY}&country=us&max=${n}`);
+
+      // Get articles from response, it's an array from GNews API, see https://gnews.io/docs/v4
       const data = response.data.articles;
       
       // Store data in cache
       cache.set(cacheKey, data);
       
+      // Return data as JSON
       res.json(data);
+
     } catch (error) {
+
+      // If there is an error, log it and return a 500 status code
       console.error(error);
       res.status(500).send('Error fetching articles');
     }
   }
 });
 
+/**
+ * GET /articles/search
+ * API endpoint to search articles from GNews API
+ * @param {string} q - search query, e.g. "Cardano"
+ */
 app.get('/articles/search', async (req, res) => {
+
+  // Get q from query string - search query
   const { q } = req.query;
   
   // Check cache first
   const cacheKey = `search_${q}`;
+
+  // Get cached data
   const cachedData = cache.get(cacheKey);
+
+  // If cached data exists, return it
   if (cachedData) {
     return res.json(cachedData);
   }
 
   try {
+    // If cached data doesn't exist, fetch data from API using axios
     const response = await axios.get(`https://gnews.io/api/v4/search?q=${q}&token=${API_KEY}`);
+
+    // Get articles from response, it's an array from GNews API, see https://gnews.io/docs/v4
     const data = response.data.articles;
     
     // Store data in cache
     cache.set(cacheKey, data);
     
+    // Return data as JSON
     res.json(data);
   } catch (error) {
+
+    // If there is an error, log it and return a 500 status code
     console.error(error);
     res.status(500).send('Error searching articles');
   }
 });
 
+/**
+ * GET /articles/:title
+ * API endpoint to search articles from GNews API by title
+ * @param {string} title - title of the article, e.g. "Cardano gets listed on Coinbase"
+ */
 app.get('/articles/:title', async (req, res) => {
 
+  // Get title from URL
   const { title } = req.params;
-  console.log("!!!!!!!!!!!!!11",title);
   
   // Check cache first
   const cacheKey = `article_${title}`;
+
+  // Get cached data
   const cachedData = cache.get(cacheKey);
+
+  // If cached data exists, return it
   if (cachedData) {
     return res.json(cachedData);
   }
 
   try {
+    // If cached data doesn't exist, fetch data from API using axios
     const response = await axios.get(`https://gnews.io/api/v4/search?q=${encodeURIComponent(title)}&token=${API_KEY}`);
+    // Get articles from response, it's an array from GNews API, see https://gnews.io/docs/v4
+    // we use find() to find the article with the title we are looking for
+    // Note we use contains instead of equals, because the title we are looking for might be a substring of the actual title
     const data = response.data.articles.find(article => article.title.includes(title));
 
     // Store data in cache
     cache.set(cacheKey, data);
     
+    // Return data as JSON
     res.json(data);
+
   } catch (error) {
+
+    // If there is an error, log it and return a 500 status code
     console.error(error);
     res.status(500).send(`Error finding article with title ${title}`);
   }
 });
 
-// Start the server
+// Start the server!
 const server = app.listen(3000, () => console.log('Server started on port 3000'));
 
 // Needed for Jest tests, see gnews-api.test.js
--- gnews-api.test.js
+++ gnews-api.test.js
@@ -1,70 +1,111 @@
+/**
+ * Unit tests for the GNews API
+ * We use Jest for testing, see https://jestjs.io/docs/getting-started
+ */
 const request = require('supertest');
 const NodeCache = require('node-cache');
 const { app, cache, server } = require('./gnews-api.js');
 
+// We need to close the server after all tests are done because otherwise the process won't exit and for example GitHub Actions will just freeze
 afterAll((done) => {
   server.close(done);
 });
 
+// Test the /articles endpoint
 describe('GET /articles', () => {
-  const n = 2;
+  const numberOfArticles = 2;
 
+  // Test that the endpoint returns an array of news articles
   it('should return an array of news articles', async () => {
-    const response = await request(app).get(`/articles?n=${n}`);
+    const response = await request(app).get(`/articles?n=${numberOfArticles}`);
+
+    // Check that the response status code is 200
     expect(response.statusCode).toBe(200);
+
+    // Check that the response body is an array
     expect(Array.isArray(response.body)).toBe(true);
+
+    // Check that the response body has the correct length
     expect(response.body.length).toBeGreaterThan(0);
   });
 
-
+  // Test that the endpoint caches the response for subsequent requests
   it('should cache the response for subsequent requests', async () => {
 
     // First request should hit the API and store the response in cache
-    const response1 = await request(app).get(`/articles?n=${n}`);
+    const response1 = await request(app).get(`/articles?n=${numberOfArticles}`);
+    // Check that the response status code is 200
     expect(response1.statusCode).toBe(200);
+    // Check that the response body is an array
     expect(Array.isArray(response1.body)).toBe(true);
-    expect(response1.body.length).toBe(n);
-    const cachedData = cache.get(`articles_${n}`);
+    // Check that the response body has the correct length
+    expect(response1.body.length).toBe(numberOfArticles);
+    // Get the cached data
+    const cachedData = cache.get(`articles_${numberOfArticles}`);
+    // Check that the cached data is the same as the response body
     expect(cachedData).toEqual(response1.body);
 
     // Second request should return the cached response
-    const response2 = await request(app).get(`/articles?n=${n}`);
+    const response2 = await request(app).get(`/articles?n=${numberOfArticles}`);
+    // Check that the response status code is 200
     expect(response2.statusCode).toBe(200);
+    // Check that the response body is an array
     expect(Array.isArray(response2.body)).toBe(true);
-    expect(response2.body.length).toBe(n);
+    // Check that the response body has the correct length
+    expect(response2.body.length).toBe(numberOfArticles);
+    // Check that the response body is the same as the first response body
     expect(response2.body).toEqual(response1.body);
   });
 });
 
-
+// Test the /articles/search endpoint
 describe('GET /articles/search', () => {
-  const q = 'technology';
 
+  // Define a query for search
+  const searchQuery = 'technology';
+
+  // Test that the endpoint returns an array of news articles that match the query
   it('should return an array of news articles that match the query', async () => {
-    const response = await request(app).get(`/articles/search?q=${q}`);
+
+    // Make a request to the endpoint
+    const response = await request(app).get(`/articles/search?q=${searchQuery}`);
+    // Check that the response status code is 200
     expect(response.statusCode).toBe(200);
+    // Check that the response body is an array
     expect(Array.isArray(response.body)).toBe(true);
+    // Check that the response body has the correct length
     expect(response.body.length).toBeGreaterThan(0);
-    expect(response.body.every(article => article.description.toLowerCase().includes(q))).toBe(true);
+    // Check that the response body contains articles that match the query
+    expect(response.body.every(article => article.description.toLowerCase().includes(searchQuery))).toBe(true);
   });
 
   it('should cache the response for subsequent requests', async () => {
 
     // First request should hit the API and store the response in cache
-    const response1 = await request(app).get(`/articles/search?q=${q}`);
+    const response1 = await request(app).get(`/articles/search?q=${searchQuery}`);
+    // Check that the response status code is 200
     expect(response1.statusCode).toBe(200);
+    // Check that the response body is an array
     expect(Array.isArray(response1.body)).toBe(true);
-    expect(response1.body.every(article => article.description.toLowerCase().includes(q))).toBe(true);
-    const cachedData = cache.get(`search_${q}`);
+    // Check that the response body contains articles that match the query
+    expect(response1.body.every(article => article.description.toLowerCase().includes(searchQuery))).toBe(true);
+    // Get the cached data
+    const cachedData = cache.get(`search_${searchQuery}`);
+    // Check that the cached data is the same as the response body
     expect(cachedData).toEqual(response1.body);
 
     // Second request should return the cached response
-    const response2 = await request(app).get(`/articles/search?q=${q}`);
+    const response2 = await request(app).get(`/articles/search?q=${searchQuery}`);
+    // Check that the response status code is 200
     expect(response2.statusCode).toBe(200);
+    // Check that the response body is an array
     expect(Array.isArray(response2.body)).toBe(true);
-    expect(response2.body.every(article => article.description.toLowerCase().includes(q))).toBe(true);
+    // Check that the response body contains articles that match the query
+    expect(response2.body.every(article => article.description.toLowerCase().includes(searchQuery))).toBe(true);
+    // Check that the response body is the same as the first response body
     expect(response2.body).toEqual(response1.body);
   });
 });
 
-// NOTE! We don't to unit testing for titles because titles seem to change all the time on their end
+// NOTE: We don't to unit testing for titles because titles seem to change all the time on their end and, 
+// it returns zero articles for a predefined query and tests fail
